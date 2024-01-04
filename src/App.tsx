2431c212abd50d4918143419101405364724875a
aqubi <msp827@naver.com>
2024-01-04 11:56:01 +0900

    Recoil: Set Selectors
    const [value, setValue] = useRecoilState(atomName);
    
    - value: atom의 값이거나, selector의 'get' 함수의 값
    - setValue: atom을 수정하는 값이거나, selector의 'set' property를 실행시키는 함수

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { minuteState, hourSelector } from './atoms';
+
+
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  const onMinutesChange = (e:React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+e.currentTarget.value); // +str => Number(str)
+  }
+  const onHoursChange = (e:React.FormEvent<HTMLInputElement>) => {
+    setHours(+e.currentTarget.value); // +str => Number(str)
+  }
+
   return (
     <div>
+      <input value={minutes} onChange={onMinutesChange} type="number" placeholder='Minutes' />
+      <input value={hours} onChange={onHoursChange} type="number" placeholder='Hours' />
     </div>
   );
 }
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,1 +1,19 @@
 import { atom, selector } from 'recoil';
+
+export const minuteState = atom({
+	key: 'minutes',
+	default: 0,
+})
+
+export const hourSelector = selector<number>({
+	key: 'hours',
+	get: ({get}) => {
+		const minutes = get(minuteState);
+		return (minutes / 60);
+	},
+	set: ({set}, newValue) => {
+		const minutes = Number(newValue) * 60;
+		set(minuteState, minutes);
+		//set(수정하고 싶은 recoil atom, 변경 후 새로운 값)
+	},
+})
